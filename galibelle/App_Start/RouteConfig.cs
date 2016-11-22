a8b49a3726bfcb2683f0bf56fe5bba3910c2c4f5
Yadira Fleitas <yadyguitar@yahoo.es>
2016-11-21 20:00:53 -0600

    Le quité el menu bar feo ese XD, estaba en la carpeta shared

--- galibelle/App_Start/RouteConfig.cs
+++ galibelle/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace galibelle
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Galibelle", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
