72e7e54650e5bf4d9c43365a34ff0432b79e3c82
Yadira Fleitas <yadyguitar@yahoo.es>
2016-11-21 19:12:43 -0600

    nuevo commit

---
+++ galibelle/Models/colores.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace galibelle.Models
+{
+    [Table("colores")]
+    public class Colores {
+        [Key]
+        public int id_colores { get; set; }
+        public string nombre_color { get; set; }
+    }
+
+}
