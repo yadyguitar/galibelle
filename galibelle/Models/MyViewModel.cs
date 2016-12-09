dcb51fe7d9f713ed1a02fafe161e9729e41afb6c
Suleyma Mota García <suley_sagitario@live.com.mx>
2016-12-08 20:34:48 -0600

    Modique los datos para que se vean en el stock

--- galibelle/Models/MyViewModel.cs
+++ galibelle/Models/MyViewModel.cs
@@ -21,7 +21,24 @@ namespace galibelle.Models
         public Ventas Ventas { get; set; }
         public Modelos Modelos { get; set; }
 
-
-       
     }
+    public class MyViewModel2
+    {
+        public Stock_straps Stock_straps { get; set; }
+        public Straps Straps { get; set; }
+        public Colores Colores { get; set; }
+        public Pedidos Pedidos { get; set; }
+        public Stock_suelas Stock_suelas { get; set; }
+        public Sucursales Sucursales { get; set; }
+        public Suelas Suelas { get; set; }
+        public Textura Textura { get; set; }
+        public Tipo_strap Tipo_strap { get; set; }
+        public Usuarios Usuarios { get; set; }
+        public Vendido Vendido { get; set; }
+        public Ventas Ventas { get; set; }
+        public Modelos Modelos { get; set; }
+
+    }
+
+
 }
