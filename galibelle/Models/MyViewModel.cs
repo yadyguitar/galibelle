047042d2d6f1b09ba9cd478c9a5f780c0ef0367a
Yadira Fleitas <yadyguitar@yahoo.es>
2016-12-08 22:48:13 -0600

    estoy hasta la madre de esto... ¬¬

--- galibelle/Models/MyViewModel.cs
+++ galibelle/Models/MyViewModel.cs
@@ -10,14 +10,16 @@ namespace galibelle.Models
         public Stock_straps Stock_straps { get; set; }
         public Straps Straps { get; set; }
         public Colores Colores { get; set; }
-        public Pedidos Pedidos { get; set; }
+        public PedidoStraps PedidoStraps { get; set; }
+        public PedidoSuelas PedidoSuelas { get; set; }
         public Stock_suelas Stock_suelas { get; set; }
         public Sucursales Sucursales { get; set; }
         public Suelas Suelas { get; set; }
         public Textura Textura { get; set; }
         public Tipo_strap Tipo_strap { get; set; }
         public Usuarios Usuarios { get; set; }
-        public Vendido Vendido { get; set; }
+        public VendidoStraps VendidoStraps { get; set; }
+        public VendidoSuelas VendidoSuelas{ get; set; }
         public Ventas Ventas { get; set; }
         public Modelos Modelos { get; set; }
 
@@ -27,14 +29,16 @@ namespace galibelle.Models
         public Stock_straps Stock_straps { get; set; }
         public Straps Straps { get; set; }
         public Colores Colores { get; set; }
-        public Pedidos Pedidos { get; set; }
+        public PedidoStraps PedidoStraps { get; set; }
+        public PedidoSuelas PedidoSuelas { get; set; }
         public Stock_suelas Stock_suelas { get; set; }
         public Sucursales Sucursales { get; set; }
         public Suelas Suelas { get; set; }
         public Textura Textura { get; set; }
         public Tipo_strap Tipo_strap { get; set; }
         public Usuarios Usuarios { get; set; }
-        public Vendido Vendido { get; set; }
+        public VendidoStraps VendidoStraps { get; set; }
+        public VendidoSuelas VendidoSuelas { get; set; }
         public Ventas Ventas { get; set; }
         public Modelos Modelos { get; set; }
 
